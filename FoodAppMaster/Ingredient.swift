5ca0f0aca85d76f9133b73bde2aabb834ee6920d
Abby Breitfeld <abby@nat-oitwireless-inside-vapornet100-d-21541.princeton.edu>
2017-07-05 17:17:56 -0400

    This is my test

--- FoodAppMaster/Ingredient.swift
+++ FoodAppMaster/Ingredient.swift
@@ -12,7 +12,7 @@ import os.log
 class Ingredient: NSObject, NSCoding {
     
     //MARK: Properties
-    
+    // FUCK CARL'S COMMENT
     var name: String
     var selected: Bool
     var shoppingListed: Bool
