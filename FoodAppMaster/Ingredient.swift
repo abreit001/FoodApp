24a9e7007a96a898cb3c8ad57cc7d8abc51b9441
CS User <csuser@nat-oitwireless-inside-vapornet100-a-20698.princeton.edu>
2017-07-06 17:11:06 -0400

    RAWR XD

--- FoodAppMaster/Ingredient.swift
+++ FoodAppMaster/Ingredient.swift
@@ -19,7 +19,7 @@ class Ingredient: NSObject, NSCoding {
     var exp: Date?
     var notificationDate: Date?
     
-    
+    // OKAY LET'S SEE IF THIS WORKS
     //MARK: Archiving Paths
     
     static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
