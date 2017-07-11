6fa444e9962a45f02ae155792d32673496836b7f
abreit001 <abreit001@mac.com>
2017-07-11 13:41:28 -0400

    Update AppDelegate.swift

--- FoodAppMaster/AppDelegate.swift
+++ FoodAppMaster/AppDelegate.swift
@@ -11,7 +11,7 @@ import UserNotifications
 import os.log
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     //CAN YOU SEE THIS LOL
     var window: UIWindow?
     let sections = ["Dairy", "Meat", "Vegetables", "Fruits", "Fish and Seafood", "Baking and Grains", "Spices and Seasonings", "Nuts and Seeds", "Legumes", "Condiments and Sauces", "Desserts and Snacks", "Soup", "Beverages and Alcohol"]
